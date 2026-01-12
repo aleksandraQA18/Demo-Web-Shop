f00ce57c8716c2ddbd453c834328fc109bc322a0
Aleksandra <a.glebocka18@gmail.com>
2026-01-12 20:53:43 +0100

    chore: eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    files: ['**/*.ts'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  {
+    rules: {
+      'no-console': 'error',
+    },
+  },
+  {
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'error',
+    },
+  },
+]);
