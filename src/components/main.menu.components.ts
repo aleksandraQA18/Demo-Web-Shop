84d759666eaf67cf742c56d4deba37c63ee50e70
Aleksandra <a.glebocka18@gmail.com>
2026-01-15 07:28:23 +0100

    feat: pom for home, books and computers

---
+++ src/components/main.menu.components.ts
@@ -0,0 +1,17 @@
+import { Locator, Page } from '@playwright/test';
+
+export class MainMenuComponent {
+  homePage: Locator;
+  booksButton: Locator;
+  computersButton: Locator;
+
+  constructor(private page: Page) {
+    this.booksButton = this.page.locator(
+      '//ul[@class="top-menu"]//a[@href="/books"]',
+    );
+    this.computersButton = this.page.locator(
+      '//ul[@class="top-menu"]//a[@href="/computers"]',
+    );
+    this.homePage = this.page.locator('.header-logo');
+  }
+}
---
+++ src/pages/base.page.ts
@@ -0,0 +1,15 @@
+import { Page } from '@playwright/test';
+
+export class BasePage {
+  url = '';
+
+  constructor(protected page: Page) {}
+
+  async goto(): Promise<void> {
+    await this.page.goto(this.url);
+  }
+
+  async getTitle(): Promise<string> {
+    return await this.page.title();
+  }
+}
