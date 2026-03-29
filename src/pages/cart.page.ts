8fdcef1ab1e89b4afd59483928f01d75b60c39ea
Aleksandra <a.glebocka18@gmail.com>
2026-03-29 14:37:32 +0200

    chore:add cart page

---
+++ src/pages/cart.page.ts
@@ -0,0 +1,37 @@
+import { TopMenuComponent } from '../components/top.menu.components';
+import { BasePage } from './base.page';
+import { Locator, Page } from '@playwright/test';
+
+export class CartPage extends BasePage {
+  productName: Locator;
+  productUnitPrice: Locator;
+  productSubtotal: Locator;
+  qtyInput: Locator;
+  updateButton: Locator;
+  itemCheckbox: Locator;
+  orderContent: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.topMenu = new TopMenuComponent(page);
+    this.productName = this.page.locator('.product-name');
+    this.productUnitPrice = this.page.locator('.product-unit-price');
+    this.productSubtotal = this.page.locator('.product-subtotal');
+    this.qtyInput = this.page.locator('.qty-input');
+    this.updateButton = this.page.locator('.update-cart-button');
+    this.itemCheckbox = this.page.locator('//input[@name="removefromcart"]');
+    this.orderContent = this.page.locator('.order-summary-content');
+  }
+
+  async editItemQty(quantity: string): Promise<void> {
+    await this.qtyInput.clear();
+    await this.qtyInput.fill(quantity);
+    await this.updateButton.click();
+  }
+
+  async deleteItem(index: number = 0): Promise<void> {
+    const item = this.itemCheckbox.nth(index);
+    await item.click();
+    await this.updateButton.click();
+  }
+}
