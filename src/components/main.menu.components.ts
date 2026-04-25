7e26ca7a92a98a22bce648008eb273f051be811f
Aleksandra <a.glebocka18@gmail.com>
2026-04-25 07:52:56 +0200

    chore: move logo locator to HomePage page

--- src/components/main.menu.components.ts
+++ src/components/main.menu.components.ts
@@ -1,12 +1,10 @@
 import { Locator, Page } from '@playwright/test';
 
 export class MainMenuComponent {
-  homePageLogo: Locator;
   booksButton: Locator;
   computersButton: Locator;
 
   constructor(private page: Page) {
-    this.homePageLogo = this.page.locator('.header-logo');
     this.booksButton = this.page.locator('.top-menu a[href="/books"]');
     this.computersButton = this.page.locator('.top-menu a[href="/computers"]');
   }
--- src/pages/home.page.ts
+++ src/pages/home.page.ts
@@ -1,10 +1,12 @@
 import { BasePage } from './base.page';
-import { Page } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 
 export class HomePage extends BasePage {
   url = '/';
+  homePageLogo: Locator;
 
   constructor(page: Page) {
     super(page);
+    this.homePageLogo = this.page.locator('.header-logo');
   }
 }
