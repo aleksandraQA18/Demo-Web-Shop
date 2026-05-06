c1ddb0a7b6639274852cc4d5ed84076b87bd1524
Aleksandra <a.glebocka18@gmail.com>
2026-05-06 19:54:29 +0200

    test: add products tests

--- src/pages/product.page.ts
+++ src/pages/product.page.ts
@@ -2,16 +2,20 @@ import { BasePage } from './base.page';
 import { CartPage } from './cart.page';
 import { Locator, Page } from '@playwright/test';
 
-export class ItemPage extends BasePage {
+export class ProductPage extends BasePage {
   addToCartButton: Locator;
   notificationBar: Locator;
   productName: Locator;
+  inStock: Locator;
+  currentPrice: Locator;
 
   constructor(page: Page) {
     super(page);
     this.addToCartButton = this.page.locator('.add-to-cart-button');
     this.notificationBar = this.page.locator('#bar-notification .content');
     this.productName = this.page.locator('.product-name h1');
+    this.inStock = this.page.locator('.stock');
+    this.currentPrice = this.page.locator('[itemprop="price"]');
   }
 
   async clickAddToCartButton(): Promise<void> {
